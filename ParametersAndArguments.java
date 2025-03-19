6aca355aa2b197696605ac43c725230c47d97c32
Nassri Lakiman <lakimannassri@bscs@plmun.edu.ph>
2025-03-19 11:34:27 +0800

    nass13

---
+++ ParametersAndArguments.java
@@ -0,0 +1,21 @@
+public class ParametersAndArguments 
+    {
+
+        public static int calculateArea(int width, int height) {
+            return width * height;
+        }
+
+            public static void main (String [] args) {
+                int roomWidth = 10;
+                int roomHeight = 15;
+            
+
+            int area = calculateArea(roomWidth, roomHeight);
+
+            System.out.println("The calculated area is: " + area);
+            
+        
+
+    }
+}
+
